33f77442f16b7a1b30b7e4b09c012fb254372f17
mikcheal101 <hirekaanmicheal@gmail.com>
2026-01-12 01:04:21 +0100

    changed the cookies

--- src/auth/constants/cookie.config.ts
+++ src/auth/constants/cookie.config.ts
@@ -2,8 +2,8 @@ import { CookieOptions } from 'express';
 
 export const ACCESS_TOKEN_COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+  secure: false,
+  sameSite: 'lax',
   path: '/',
   maxAge: 1000 * 60 * 60,
 };
