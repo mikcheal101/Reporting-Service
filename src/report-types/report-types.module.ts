7e1a3358ddfb86688163ca389289e9227fbc1217
mikcheal101 <hirekaanmicheal@gmail.com>
2026-01-17 21:29:30 +0100

    added task to reporttype

--- src/report-types/report-types.module.ts
+++ src/report-types/report-types.module.ts
@@ -5,9 +5,10 @@ import { ReportTypesService } from './report-types.service';
 import { ReportTypesController } from './report-types.controller';
 import { ReportTypeUtils } from './utils/report-type.utils';
 import { CronUtil } from 'src/common/utils/cron.utils';
+import { Task } from 'src/tasks/entity/task.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ReportType])],
+  imports: [TypeOrmModule.forFeature([ReportType, Task])],
   providers: [ReportTypesService, ReportTypeUtils, CronUtil],
   controllers: [ReportTypesController],
   exports: [ReportTypesService],
